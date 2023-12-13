6b56936e09946ef6241ac95a1335f4491dcbfd34
EtinRuyi <omoruyi.idowu@yahoo.com>
2023-12-13 09:09:10 +0100

    UpdatedApp

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React, { Suspense } from 'react';
 import ErrorBoundary from './components/common/ErrorBoundary';
-//import EmailSent from './pages/EmailSent';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import AuthLayout from './layouts/AuthLayout';
-// import Landing from './pages/Landing';
 import { GlobalProvider } from './context/GlobalState';
 import AuthState from './context/auth/AuthState';
 import 'react-toastify/dist/ReactToastify.css';
